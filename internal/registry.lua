1d56b159dae81ac26fd1bbba19d957cc02f6f4ab
JakeyRoundHead <112113737+J4KEWasNotHere@users.noreply.github.com>
2026-08-12 16:40:42 +0200

    Add Registry

---
+++ internal/registry.lua
@@ -0,0 +1,14 @@
+return {
+	VideWallyDirectory = "centau_vide@",
+	VideVersions = {
+		["0.4.1"] = {
+			order = 1,
+			url = "https://github.com/centau/vide/releases/download/0.4.1/build.rbxm",
+		},
+
+		["0.4.0"] = {
+			order = 0,
+			url = "https://github.com/centau/vide/releases/download/0.4.0/build.rbxm",
+		},
+	},
+}
